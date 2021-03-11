81b27eb239eb8a15a626143206bc9ae6e7679969
JuliaSleptsova <yuli.sleptsova@gmail.com>
2021-03-11 07:37:04 +0300

    9.3. Преобразование примитивных типов [#492329]

---
+++ src/ru/job4j/DataTypes.java
@@ -0,0 +1,32 @@
+package ru.job4j;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        // 1 выражение
+        long l = 129;
+        byte b = (byte) l;
+        System.out.println(b);
+
+// 2 выражение
+        float f = 12358.7f;
+        int i = 45981;
+        short rsl = (short) (f + i);
+        System.out.println(rsl);
+
+// 3 выражение
+        char c = 45000;
+        float n = c;
+        System.out.println(n);
+
+// 4 выражение
+        double d = 121.19;
+        byte z = (byte) d;
+        System.out.println(z);
+
+// 5 выражение
+        short s = 1500;
+        char p = (char) s;
+        double q = p;
+        System.out.println(q);
+    }
+}
