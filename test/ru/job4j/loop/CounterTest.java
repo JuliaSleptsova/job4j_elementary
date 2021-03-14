c931cf8d08efae4d48648dfdb902595601d7ff3b
JuliaSleptsova <yuli.sleptsova@gmail.com>
2021-03-14 19:42:56 +0300

    5.0.1. Сумма чисел. [#492379]

---
+++ test/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,36 @@
+package ru.job4j.loop;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CounterTest {
+
+    @Test
+    public void whenStart3Finish8Them33() {
+        int start = 3;
+        int finish = 8;
+        int expected = 33;
+        int out = Counter.sum(start, finish);
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void whenStart0Finish5Them15() {
+        int start = 0;
+        int finish = 5;
+        int expected = 15;
+        int out = Counter.sum(start, finish);
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void whenStart9Finish80Them3204() {
+        int start = 9;
+        int finish = 80;
+        int expected = 3204;
+        int out = Counter.sum(start, finish);
+        Assert.assertEquals(expected, out);
+    }
+}
