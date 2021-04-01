2eaaf89a92b876f7cae1679e08dee2ffdf302765
JuliaSleptsova <yuli.sleptsova@gmail.com>
2021-04-01 13:17:13 +0300

    2. Модель данных. [#492497]

---
+++ src/ru/job4j/pojo/College.java
@@ -0,0 +1,13 @@
+package ru.job4j.pojo;
+
+import java.util.Date;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setFullName("Ivan Ivanov");
+        student.setGroup(11);
+        student.setDate(new Date());
+        System.out.println("Full name: " +  student.getFullName() + System.lineSeparator() + "Group: " + student.getGroup() + System.lineSeparator() + "Data: " + student.getDate());
+    }
+}
